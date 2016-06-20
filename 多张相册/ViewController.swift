a9a3fa36bbd0b840ade63120083a88feb5cab6fa
laidanzhang <417488675@qq.com>
2016-06-20 18:41:17 +0800

    Initial Commit

---
+++ 多张相册/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  多张相册
+//
+//  Created by 马江海 on 16/6/20.
+//  Copyright © 2016年 马江海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
